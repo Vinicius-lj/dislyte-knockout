0c1728cb178cf81f4ba788c7c585b5c50240e4d6
Vinicius Leoncio <vini.leo.juca@gmail.com>
2023-12-20 23:30:42 -0300

    excluded polls app config

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('dislyte_knockout/', include('dislyte_knockout.urls')),
     path('admin/', admin.site.urls),
 ]
